fadfd1839bc7e1394b03afd64378a647574c37a2
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-10-01 20:56:04 +0200

    Add responsive design for about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,6 +2,9 @@ const m = require("mithril");
 const Layout = require('../layout/Layout');
 const Navigation = require('../navigation/Navigation');
 
+let resume = require('../../../datas/resume.json');
+let about = resume.about;
+
 function About() {
     return {
         view: () => (
@@ -11,14 +14,7 @@ function About() {
                 </header>
                 <section id="about">
                     <h2>A propos</h2>
-                    <p class="text-long">
-                        Je suis Jérémy Deurvillier, Développeur Web depuis 2017. Je suis un passionné du code.
-                        J'aime apprendre et découvrir de nouvelles choses, dans tous les domaines.
-                        Les gros projets ne me font pas peur, j'aime les challenges
-                        et me donner à cent pour cent est une habitude de tous les jours.
-                        J'aime explorer les 64 cases du jeu d'échecs régulièrement,
-                        le karaté depuis de nombreuses années et enfin la randonnée, activité dont je ne me lasse jamais.
-                    </p>
+                    { about.map(text => <p class="text-long">{ text }</p>) }
                     <p class="about-cta">
                         <m.route.Link href="/projects" class="button">Voir mes projets</m.route.Link>
                     </p>
