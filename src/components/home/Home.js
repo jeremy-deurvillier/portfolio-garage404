37d2c094efc9af3fb4cb616c0d9d15dbcc9f920b
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-10-04 11:34:58 +0200

    Get informations from json file on Home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,6 +2,10 @@ const m = require("mithril");
 const Layout = require('../layout/Layout');
 const Navigation = require('../navigation/Navigation');
 
+let resume = require('../../../datas/resume.json');
+let identity = resume.identity;
+let job = resume.job;
+
 function Home() {
     return {
         view: () => (
@@ -11,8 +15,8 @@ function Home() {
                 </header>
                 <section id="home">
                     <p class="hi">Salut, je suis</p>
-                    <h2>Jérémy Deurvillier</h2>
-                    <h3>Développeur Web <span>passionné</span></h3>
+                    <h2>{ identity.name.first + ' ' + identity.name.last }</h2>
+                    <h3>{ job.title } <span>passionné</span></h3>
                     <p class="home-cta">
                         <m.route.Link href="/about" class="button">Découvrir</m.route.Link>
                     </p>
